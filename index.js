f18630e9a09acc3250a5ff8dac9c4099d9a556e5
João Victor <soulima.joao@gmail.com>
2021-10-20 15:03:19 -0300

    bootstrap

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
-const app = express()
+const server = express()
 
 //express usando o EJS como renderizador de HTML
-app.set('view engine', 'ejs')
+server.set('view engine', 'ejs')
+server.use(express.static('public')) //pasta que ficarão arquivos utilizados apenas no front
 
-app.get('/', (req, res) => {
-    res.render('index') //renderiza arquivos html
+server.get('/:name/:lang', (req, res) => {
+    const name = req.params.name
+    const lang = req.params.lang
+    let msg = false
+
+    res.render('index', {
+        name: name,
+        lang: lang,
+        msg: msg
+    }) //renderiza arquivos html
 })
 
-app.listen(3000, (error) => {
+server.listen(3000, (error) => {
     if (error) {
         console.log(`o ocorreu um (ou mais) erro: ${error}`)
     } else {
